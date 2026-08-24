2d8493f962bfeba6ea9f7c6ce80e419ba60eda53
Regan Hutson <hutson.regan@gmail.com>
2026-08-23 20:49:05 -0700

    Copying all from original map.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portland-pastry-10k
+
+Web-map for the inagural Portland Pastry 10K.
---
+++ data/vendors.js
@@ -0,0 +1,11 @@
+var vendors= {
+"type": "FeatureCollection",
+"name": "pastry-10k-vendors",
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "Name": "Harder Day Coffee", "Address": "2031 NE MLK Jr Blvd<br> Portland, OR 97212", "Website": "https://www.harderdaycoffeecompany.com", "Instagram": "https://www.instagram.com/harderdaycoffeeco/", "Info": "Pastry by <a href=https://www.instagram.com/musettepdx/> Musette </a>" }, "geometry": { "type": "Point", "coordinates": [ -122.6617277, 45.537647 ] } },
+{ "type": "Feature", "properties": { "fid": 2, "Name": "Caffe Destino", "Address": "1339 NE Fremont <br>Portland, OR 97212", "Website": "https://www.caffedestinopdx.com", "Instagram": "https://www.instagram.com/caffedestinopdx/", "Info": "null" }, "geometry": { "type": "Point", "coordinates": [ -122.6519611, 45.5483046 ] } },
+{ "type": "Feature", "properties": { "fid": 3, "Name": "Tiny Love Bakery", "Address": "5124 NE 42nd Ave <br>Portland,  OR 97218", "Website": "https://tinylovebakery.com/", "Instagram": "https://www.instagram.com/tinylovebakerypdx/", "Info": "null" }, "geometry": { "type": "Point", "coordinates": [ -122.6202767, 45.5599091 ] } },
+{ "type": "Feature", "properties": { "fid": 4, "Name": "NGOT", "Address": "null", "Website": "https://ngotpdx.com", "Instagram": "https://www.instagram.com/ngotpdx/", "Info": "null" }, "geometry": { "type": "Point", "coordinates": [ -122.6323031, 45.5481964 ] } },
+{ "type": "Feature", "properties": { "fid": 5, "Name": "Lasagna Project", "Address": "null", "Website": "https://linktr.ee/lasagnaprojectpdx", "Instagram": "https://www.instagram.com/lasagnaprojectpdx/", "Info": "null" }, "geometry": { "type": "Point", "coordinates": [ -122.6449041, 45.5390514 ] } }
+]
+}
